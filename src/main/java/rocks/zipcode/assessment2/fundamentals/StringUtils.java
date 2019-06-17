f2af6e402de8f6437d71aa42aec59506edf34d41
Stefun <hawkd215@gmail.com>
2019-06-17 14:05:48 -0400

    74

--- src/main/java/rocks/zipcode/assessment2/fundamentals/StringUtils.java
+++ src/main/java/rocks/zipcode/assessment2/fundamentals/StringUtils.java
@@ -59,9 +59,9 @@ public class StringUtils {
      * @return - true if string only contains special characters
      */
     public static Boolean isSpecialCharacterString(String string) {
-        String specialChaacters = "[" +"-/@#!*$%^&_+={"();
+        String specialChaacters = "[" +"-/@#!*$%^&_+={";
 
-        if));
+
         return true;
     }
 }
--- src/main/java/rocks/zipcode/assessment2/objectorientation/Address.java
+++ src/main/java/rocks/zipcode/assessment2/objectorientation/Address.java
@@ -1,11 +1,26 @@
 package rocks.zipcode.assessment2.objectorientation;
 
+import java.util.Comparator;
+
 /**
  * @author leon on 28/11/2018.
  */
-public class Address {
+public class Address  {
+    private String addressLine1;
+    private String addressLine2;
+    private String city;
+    private String state;
+    private String zipcode;
+
     public Address() {
+        this.addressLine1 = "123 Gitlabs";
+        this.addressLine2 = "";
+        this.city = "New Castle";
+        this.state = "Delaware";
+        this.zipcode = "19720";
     }
+
+
 
     /**
      * @param addressLine1 - first address line
@@ -15,42 +30,57 @@ public class Address {
      * @param zipcode - zipcode of region
      */
     public Address(String addressLine1, String addressLine2, String city, String state, String zipcode) {
+        this.addressLine1 = "123 Gitlabs";
+        this.addressLine2 = "";
+        this.city = "New Castle";
+        this.state = "Delaware";
+        this.zipcode = "19720";
     }
 
     public String getAddressLine1() {
-        return null;
+        return addressLine1;
     }
 
     public void setAddressLine1(String addressLine1) {
+        this.addressLine1 = addressLine1;
     }
 
     public String getAddressLine2() {
-        return null;
+        return addressLine2;
     }
 
     public void setAddressLine2(String addressLine2) {
+        this.addressLine2 = addressLine2;
     }
 
     public String getCity() {
-        return null;
+        return city;
     }
 
     public void setCity(String city) {
+        this.city = city;
     }
 
     public String getState() {
-        return null;
+        return state;
     }
 
     public void setState(String state) {
+        this.state = state;
     }
 
     public String getZipcode() {
-        return null;
+        return zipcode;
     }
 
     public void setZipcode(String zipcode) {
+        this.zipcode = zipcode;
     }
+
+//    @Override
+//    public int compare(Object o1, Object o2) {
+//        return o1.().compareTo(o2.());
+
 
     @Override
     public boolean equals(Object o) {
